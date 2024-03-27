0977745c4c970d1f8d1152398e528cbb4c01afc0
Alekseyua <alekseyuadnepr@gmail.com>
2024-03-27 23:04:02 +0200

    first commit admin v3

--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,22 @@
 import axios from "axios";
 
 
- class Api {
+
+
+class Api {
   HOST = 'http://193.0.61.232:'  //process.env.NODE_ENV !== 'development'? process.env.REACT_APP_HOST_DEV : process.env.REACT_APP_HOST_PROD;
   PORT = 7000; //process.env.REACT_APP_SERVER_PORT
+  createAxios = axios.create({
+    headers: { 
+      'x-apikey': '59a7ad19f5a9fa0808f11931',
+      'Access-Control-Allow-Origin' : '*',
+      'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    },
+  })
   async get(url, params){
     try {      
       // console.log({params})
-      const res = await axios.get(this.HOST + this.PORT + url, {params})
+      const res = await this.createAxios.get(this.HOST + this.PORT + url, {params})
       return res.data;
     } catch (error) {
       throw error;  
@@ -21,7 +30,7 @@ import axios from "axios";
           'Content-Type': 'multipart/form-data',
         }
       };
-      const res = await axios.post(this.HOST + this.PORT + url, params, config)
+      const res = await this.createAxios.post(this.HOST + this.PORT + url, params, config)
       return res.data
     } catch (error) {
       throw error
@@ -32,7 +41,7 @@ import axios from "axios";
   // console.log({params})
 
     try {      
-      const res = await axios.put(this.HOST + this.PORT + url, params)
+      const res = await this.createAxios.put(this.HOST + this.PORT + url, params)
       return res.data;
     } catch (error) {
       throw error;  
